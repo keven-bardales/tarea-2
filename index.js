a859c4211450e6d71d3ae3b27fc001ec84e50abf
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-07-30 23:00:59 -0600

    crud operations

--- index.js
+++ index.js
@@ -16,33 +16,87 @@ class Pelicula {
       this.peliculas.push(pelicula);
     }
 
+    eliminarPelicula(titulo) {
+      this.peliculas = this.peliculas.filter((pelicula) => pelicula.titulo !== titulo);
+    }
+
     obtenerTodasLasPeliculas() {
       return this.peliculas;
+    }
+
+    editarPelicula(indice, pelicula) {
+      this.peliculas[indice] = pelicula;
     }
   }
 
   const coleccion = new ColeccionPeliculas();
 
+  let indiceEdicion = null;
+
   function agregarPelicula() {
+    const formValues = document.querySelectorAll('input')
+    let allInputsFilled = true
+      formValues.forEach((val) => {
+        console.log(val.value)
+        if(val.value.length === 0) {
+            allInputsFilled = false
+        }
+    })
+
+
+    if(!allInputsFilled) return
+
     const titulo = document.getElementById('titulo').value;
     const genero = document.getElementById('genero').value;
     const anio = document.getElementById('anio').value;
     const director = document.getElementById('director').value;
 
     const pelicula = new Pelicula(titulo, genero, anio, director);
-    coleccion.agregarPelicula(pelicula);
+    if (indiceEdicion !== null) {
+      coleccion.editarPelicula(indiceEdicion, pelicula);
+      indiceEdicion = null;
+    } else {
+      coleccion.agregarPelicula(pelicula);
+    }
 
     mostrarPeliculas();
     limpiarCampos();
+  }
+
+  function eliminarPelicula(titulo) {
+    coleccion.eliminarPelicula(titulo);
+    mostrarPeliculas();
+  }
+
+  function editarPelicula(indice) {
+    const pelicula = coleccion.obtenerTodasLasPeliculas()[indice];
+    document.getElementById('titulo').value = pelicula.titulo;
+    document.getElementById('genero').value = pelicula.genero;
+    document.getElementById('anio').value = pelicula.anio;
+    document.getElementById('director').value = pelicula.director;
+    indiceEdicion = indice;
   }
 
   function mostrarPeliculas() {
     const listaPeliculas = document.getElementById('lista-peliculas');
     listaPeliculas.innerHTML = '';
 
-    coleccion.obtenerTodasLasPeliculas().forEach((pelicula) => {
+    coleccion.obtenerTodasLasPeliculas().forEach((pelicula, index) => {
       const li = document.createElement('li');
       li.textContent = `${pelicula.titulo} - ${pelicula.genero} - ${pelicula.anio} - ${pelicula.director}`;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Eliminar';
+      deleteButton.classList.add('delete-button');
+      deleteButton.onclick = () => eliminarPelicula(pelicula.titulo);
+      li.appendChild(deleteButton);
+
+      const editButton = document.createElement('button');
+      editButton.onclick = () => editarPelicula(index);
+      editButton.textContent = 'Editar';
+      editButton.classList.add('edit-button');
+      li.appendChild(editButton);
+
       listaPeliculas.appendChild(li);
     });
   }
